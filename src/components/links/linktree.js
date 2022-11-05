f2e05579e1d07ecee5e4833079343ef1df09faf2
Abdurahmon Abdulhakeem Adeolaking <53256584+Adeolaking@users.noreply.github.com>
2022-11-05 22:00:20 +0100

    Fixes: added contact link class

--- src/components/links/linktree.js
+++ src/components/links/linktree.js
@@ -58,7 +58,7 @@ function LinkTree() {
             {link.name}
           </a>
         ))}
-       <Link to="/contact" className='links md:px-16 sm:px-12 xsm:px-2 px-2 w-full mt-5' id="contact">Contact</Link>
+       <Link to="/contact"  className='block w-full text-center text-[#101828] font-medium sm:text-lg xsm:text-base text-sm py-[20px] mb-5 bg-[#EAECF0] hover:bg-[#D0D5DD] rounded-lg border-1 border-[ #EAECF0]' id="contact">Contact</Link>
       </div>
 
       <div className='social flex mt-4 px-4'>
